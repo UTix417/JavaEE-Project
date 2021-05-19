ce430e46ed2feed7efd66717c135f91cf63dc039
2561101729 <83761703+2561101729@users.noreply.github.com>
2021-05-19 11:22:49 +0800

    Add files via upload

---
+++ code/Blog/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.pojo.User;
+import com.example.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author 朱威
+ * @create 2021-05-18-20:57
+ */
+//本类实现用户登录以及注册的业务
+@Controller
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+    @Autowired
+    UserMapper userMapper;
+    @RequestMapping("/login")
+    public String userLogin(int userId, String userPassword, HttpSession session, Model model){
+        User user = loginService.login(userId, userPassword);
+        if(user != null)
+        {
+            session.setAttribute("user",user);
+            return "redirect:/user/getAllBlock";
+        }
+        else
+        {
+            model.addAttribute("msg","用户名或密码错误");
+            return "index";
+        }
+    }
+
+    @RequestMapping("/register")
+    public String userRegister(String userName,String userPassword){
+        User user = new User(null, userName, userPassword, 1, new Date(System.currentTimeMillis()), null);
+        userMapper.addUser(user);
+        return "redirect:/user/getAllBlock";
+    }
+}
---
+++ code/Blog/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.BlockMapper;
+import com.example.demo.pojo.Block;
+import com.example.demo.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * @author 朱威
+ * @create 2021-05-19-9:02
+ */
+//本类实现普通用户的功能
+    @Controller
+    @RequestMapping("/user")
+public class UserController {
+    @Autowired
+    BlockMapper blockMapper;
+
+    //本方法实现登录后返回所有的板块信息
+    @RequestMapping("/getAllBlock")
+    public String getAllBlock(Model model, HttpSession session){
+        User user = (User) session.getAttribute("user");
+        List<Block> blocks = blockMapper.getBlockByLevel(user.getUserLevel());
+        model.addAttribute("blocks",blocks);
+        return "blocks";//这里返回登录成功后的页面
+    }
+}
