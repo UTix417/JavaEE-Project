ada4880b2027ffe0f1a2304781343b1feb0253dc
“EKvince” <1015199227@qq.com>
2021-05-26 10:29:35 +0800

    Revert "新增以下函数的ctroller"
    This reverts commit 7cf6344d168d44ff8b2255edf7a40aee5f90a05a.
    
    # Conflicts:
    #	code/Blog/.idea/libraries/Maven__com_alibaba_fastjson_1_2_66.xml
    #	code/Blog/src/main/java/com/example/demo/config/MyMVCConfig.java
    #	code/Blog/src/main/java/com/example/demo/controller/BlockController.java
    #	code/Blog/src/main/java/com/example/demo/controller/PageController.java
    #	code/Blog/src/main/java/com/example/demo/controller/UserController.java
    #	code/Blog/src/main/java/com/example/demo/mapper/BlockMapper.java
    #	code/Blog/src/main/java/com/example/demo/mapper/UserMapper.java
    #	code/Blog/src/main/resources/mybatis/mapper/BlockMapper.xml
    #	code/Blog/src/main/resources/mybatis/mapper/UserMapper.xml
    #	code/Blog/target/classes/com/example/demo/controller/BlockController.class
    #	code/Blog/target/classes/com/example/demo/controller/InviteController.class
    #	code/Blog/target/classes/com/example/demo/controller/PageController.class
    #	code/Blog/target/classes/com/example/demo/controller/UserController.class
    #	code/Blog/target/classes/com/example/demo/mapper/BlockMapper.class
    #	code/Blog/target/classes/com/example/demo/mapper/InviteCodeMapper.class
    #	code/Blog/target/classes/com/example/demo/mapper/PageMapper.class
    #	code/Blog/target/classes/com/example/demo/mapper/TextMapper.class
    #	code/Blog/target/classes/com/example/demo/mapper/UserMapper.class

---
+++ code/Blog/src/main/java/com/example/demo/config/MyMVCConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 朱威
+ * @create 2021-05-19-9:57
+ */
+@Configuration
+public class MyMVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html","/login","/","/img/**","/webjars/**");
+    }
+}
