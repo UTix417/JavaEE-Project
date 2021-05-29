65039429b60a4acf6c00436ab58ccd582f2e40ba
UTix417 <83760659+UTix417@users.noreply.github.com>
2021-05-29 17:19:46 +0800

    修复了某些情况下不能发帖的BUG

--- code/Blog/src/main/java/com/example/demo/controller/TextPublishController.java
+++ code/Blog/src/main/java/com/example/demo/controller/TextPublishController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -37,23 +38,31 @@ public class TextPublishController {
         System.out.println("In TextPublishController controller");
         User now_user= (User) session.getAttribute("user");
         String title=String.valueOf(map.get("title"));
+        session.setAttribute("blockid",1);
         String content= String.valueOf(map.get("content"));
         int blockid= (int) session.getAttribute("blockid");
         System.out.println(title);
         System.out.println(content);
         Text text=new Text();
+        List<Text> texts = textMapper.getTextsByBlockId(blockid);
+        Integer textId= texts.size();
+        text.setTextId(textId);
         text.setTextTitle(title);
         text.setTextStatus(1);
         text.setTextCreateTime(new Date());
         text.setUser(now_user);
         text.setBlockId(blockid);//Blocks存session
+        System.out.println(text);
         textMapper.addText(text);
         Page page=new Page();
+        page.setTextId(textId);
         page.setPageFloor(0);
         page.setPageContent(content);
         page.setUser(now_user);
         page.setPageRetime(new Date());
+        System.out.println(page);
         pageMapper.addPage(page);
+        System.out.println("end");
         return "发帖成功";//返回首页?个人页？
     }
 }
