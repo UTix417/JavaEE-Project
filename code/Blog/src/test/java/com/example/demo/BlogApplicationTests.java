aa4c3a53827c60c3c4387d1c0bf9727f8eeda949
2561101729 <83761703+2561101729@users.noreply.github.com>
2021-05-29 12:31:04 +0800

    修改了messagemapper

--- code/Blog/src/test/java/com/example/demo/BlogApplicationTests.java
+++ code/Blog/src/test/java/com/example/demo/BlogApplicationTests.java
@@ -26,20 +26,21 @@ class BlogApplicationTests {
     @Test
     void contextLoads() {
 //        System.out.println(userMapper.getUserById(1));
-        User user1 = new User(1, "shit", "654321", 0, 0,new Date(System.currentTimeMillis()), "pppp");
-        User user2 = new User(2, "shit", "654321", 0,0, new Date(System.currentTimeMillis()), "pppp");
-        User user3 = new User(null, "user5", "654321", 0,0, new Date(System.currentTimeMillis()), "path");
-        User user4 = new User(9, "newuser", "123456", 0,1, new Date(System.currentTimeMillis()), "path");
-        Block block = new Block(null, "修改后的板块2", null, null);
-        Text text = new Text(8,"芜湖起飞",2,new Date(System.currentTimeMillis()),1,user1);
-        Page page = new Page(1, 5, new Date(System.currentTimeMillis()), "我是第五", new User(4, null, null,null, null, null, null));
-        Message message = new Message(null, user1, user2, new Date(System.currentTimeMillis()), "你好");
-        InviteCode inviteCode = new InviteCode(null,"ASDFGHJ",null,1,2,new Date(System.currentTimeMillis()));
-        List<Text> text1 = textMapper.getTextsByBlockId(1,1);
-        List<Text> text2 = textMapper.getTextsByBlockId(1,2);
-        System.out.println(text2);
-        text2.addAll(text1);
-        System.out.println(text2);
+//        User user1 = new User(1, "shit", "654321", 0, 0,new Date(System.currentTimeMillis()), "pppp");
+//        User user2 = new User(2, "shit", "654321", 0,0, new Date(System.currentTimeMillis()), "pppp");
+//        User user3 = new User(null, "user5", "654321", 0,0, new Date(System.currentTimeMillis()), "path");
+//        User user4 = new User(9, "newuser", "123456", 0,1, new Date(System.currentTimeMillis()), "path");
+//        Block block = new Block(null, "修改后的板块2", null, null);
+//        Text text = new Text(8,"芜湖起飞",2,new Date(System.currentTimeMillis()),1,user1);
+//        Page page = new Page(1, 5, new Date(System.currentTimeMillis()), "我是第五", new User(4, null, null,null, null, null, null));
+//        Message message = new Message(null, user1, user2, new Date(System.currentTimeMillis()), "你好");
+//        InviteCode inviteCode = new InviteCode(null,"ASDFGHJ",null,1,2,new Date(System.currentTimeMillis()));
+//        List<Text> text1 = textMapper.getTextsByBlockId(1,1);
+//        List<Text> text2 = textMapper.getTextsByBlockId(1,2);
+//        System.out.println(text2);
+//        text2.addAll(text1);
+//        System.out.println(text2);
+        System.out.println(messageMapper.getAllMessageByUserId(1, 2));
 //        System.out.println(userMapper.getUserById(1));
 //        userMapper.updateUser(user);
 //        System.out.println(blockMapper.getAllBlocks());
