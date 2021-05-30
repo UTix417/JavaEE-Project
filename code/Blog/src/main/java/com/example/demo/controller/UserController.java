1814e9ed3fc8667fbd51754c4058793d2013a869
王令 <53396287+EKvince@users.noreply.github.com>
2021-05-30 22:32:57 +0800

    更新BanUser
    只需要传入ID就可以默认Ban一天，权限要在3以上

--- code/Blog/src/main/java/com/example/demo/controller/UserController.java
+++ code/Blog/src/main/java/com/example/demo/controller/UserController.java
@@ -130,14 +130,22 @@ public class UserController {
 //    }
 
     //本方法用来封禁一个用户请传入用户Id和解封时间,-1代表封禁失败
+    //传入一个用户的ID，直接禁言1天
     @ResponseBody
     @RequestMapping("/banUser/{id}")
-    public int banUser(@PathVariable("id") int userId,Date outTime,HttpSession session){
+    public int banUser(@PathVariable("id") int userId,HttpSession session){
         User now_user= (User) session.getAttribute("user");
         Integer userLevel = now_user.getUserLevel();
+        System.out.println(userLevel);
+
+        Date date = new Date(); //取时间
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        calendar.add(calendar.DATE,1); //把日期往后增加一天,整数  往后推,负数往前移动
+        date=calendar.getTime(); //这个时间就是日期往后推一天的结果
+
         if (userLevel<3)//3等级以上的人才能BAN用户
             return -1;
-        return userMapper.banUser(userId,outTime);
+        return userMapper.banUser(userId,date);
     }
-
 }
